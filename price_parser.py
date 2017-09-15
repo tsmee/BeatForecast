d5d609f663382c49c73b8f778513201913f57414
Mike Azarov <draftpodium@gmail.com>
2017-09-16 02:56:22 +0700

    slightly improved string procession

--- price_parser.py
+++ price_parser.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.discogs.com/Not-Waving-Animals/release/8092763")
+page = requests.get("https://www.discogs.com/Consumer-Electronics-Repetition-Reinforcement-/release/6724351")
 soup = BeautifulSoup(page.content, 'html.parser')
 a = soup.find('ul', class_='last')
 
 z = str(a.get_text())
 
 zz = z.split("\n")
-xx = []
-for n in zz:
-    xx.append(n.lstrip())
-xx = [x for x in xx if x]
 
-print (xx)
+#после удаления \n в списке остаются элементы из одних пробелов и пустые элементы. удаляем их.
+zzz = [x.strip() for x in zz if not all(c == ' ' for c in x)]
+
+
+
+
+
+
